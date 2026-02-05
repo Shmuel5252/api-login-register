695489960fa7ae40e1bb5f16f91f6f1cfb4c9b42
Shmuel5252 <shmuel.5425252@gmail.com>
2026-02-05 15:27:43 +0200

    Connect MongoDB and add User model

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-require('dotenv').config({quiet: true});
+require('dotenv').config({ quiet: true });
 
 const express = require('express')
+const mongoose = require('mongoose');
 
 const app = express();
-
 app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.json({message: 'API is running'})
+app.get('/', (req, res) => {
+    res.json({ message: 'API is running' })
 });
 
 const PORT = process.env.PORT || 3001;
+const MONGO_URI = process.env.MONGO_URI;
 
-app.listen (PORT, ()=>{
-    console.log(`✅ Server is running on port ${PORT}`);
-})
+async function start() {
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log('✅ Connected to MongoDB');
+        
+
+        app.listen(PORT, () => {
+            console.log(`✅ Server is running on port ${PORT}`);
+        });
+    }catch(error){
+        console.log('❌ Failed to start: ', error.message);
+        process.exit(1);
+    }
+}
+
+start();
